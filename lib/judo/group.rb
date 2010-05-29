b1295175a18012d5f596e9f9971c341ba04ac05f
Peter van Hardenberg <pvh@heroku.com>
2010-05-28 22:05:21 -0400

    gutted group like a fish.

--- lib/judo/group.rb
+++ lib/judo/group.rb
@@ -12,10 +12,6 @@ module Judo
       @config ||= load_config
     end
 
-    def build_config
-      @build_config ||= all_configs.reverse.inject({}) { |sum,conf| sum.merge(conf) }
-    end
-
     def server_ids
       @server_ids ||= (@base.groups_config[@name] || [])
     end
@@ -40,27 +36,14 @@ module Judo
         @version = @version + 1
         FileUtils.rm_rf(tmpdir)
         FileUtils.mkdir_p(tmpdir)
-        new_config = build_config
-        Dir.chdir(tmpdir) do |d|
-          attachments(new_config).each do |to,from|
-            FileUtils.mkdir_p(File.dirname(to))
-            if from =~ /^http:\/\//
-              # puts "curl '#{from}'"
-              system "curl '#{from}' > #{to}"
-              # puts "#{to} is #{File.stat(to).size} bytes"
-            else
-              FileUtils.cp(from,to)
-            end
-          end
-          File.open("config.json", "w") { |f| f.write(new_config.to_json) }
-          Dir.chdir("..") do
+        new_config = raw_config
+        Dir.chdir(@base.repo) do |d|
             puts ""
             system "tar czvf #{tar_file} #{name}"
             puts "Uploading config to s3..."
             @base.s3_put(version_config_file, new_config.to_json)
             puts "Uploading tar file to s3..."
             @base.s3_put(tar_file, File.new(tar_file).read(File.stat(tar_file).size))
-          end
         end
         set_version
       end
@@ -78,65 +61,9 @@ module Judo
       @url = @base.s3_url(tar_file)
     end
 
-    def cp_file
-      FileUtil.mkdir_p(tmpdir)
-    end
-
-    def extract_file(type, name, files)
-      path = "#{dir}/#{type}s/#{name}"
-      found = Dir[path]
-      if not found.empty?
-        found.each { |f| files["#{type}s/#{File.basename(f)}"] = f }
-      elsif parent
-        parent.extract_file(type, name, files)
-      else
-        raise "Cannot find file #{name} of type #{type}"
-      end
-    end
-
-    def extract(config, files)
-      config.each do |key,value|
-        [value].flatten.each do |v|   ### cover "packages" : ["a","b"], "packages" : "a", "packages":[{ "file" : "foo.pkg"}]
-          if v.is_a? Hash
-            extract(v, files)
-          else
-            case key
-            when *[ "init", "before", "after" ]
-              extract_file(:script, v, files) unless v =~ /^#!/
-            when "package"
-              files["packages/#{v}_i386.deb"]  = "#{config["source"]}#{v}_i386.deb"
-              files["packages/#{v}_amd64.deb"] = "#{config["source"]}#{v}_amd64.deb"
-            when "local_packages"
-              extract_file(:package, "#{v}_i386.deb", files)
-              extract_file(:package, "#{v}_amd64.deb", files)
-            when "git"
-              ## do nothing - is a remote
-            when "template"
-              extract_file(:template, v, files)
-            when "source"
-              extract_file(:file, v, files) unless config["template"] or config["package"]
-            when "file"
-              ## this is getting messy
-              extract_file(:file, File.basename(v), files) unless config["template"] or config["source"] or config["git"]
-            end
-          end
-        end
-      end
-      files
-    end
-
-    def attachments(c = config)
-      extract(c, {})
-    end
 
     def raw_config
       @raw_config ||= read_config
-    end
-
-    def parent
-      @parent ||= @base.groups.detect { |p| p.name == raw_config["import"] }
-      (@parent ||= Judo::Group.new(@base, raw_config["import"], 0 )) if raw_config["import"]  ## make a mock incase the parent was never committed
-      @parent
     end
 
     def destroy
@@ -146,10 +73,6 @@ module Judo
         @base.sdb.delete_attributes(@base.base_domain, "groups", [ name ])
         @base.sdb.delete_attributes(@base.base_domain, "group_versions", [ name ])
       end
-    end
-
-    def all_configs
-      parent ? parent.all_configs.clone.unshift(raw_config) : [ raw_config ]
     end
 
     def dir
