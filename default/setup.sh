f7a3abd56618fe7f2ca732fb1082f950ef5b3ce1
Mark McGranaghan <mmcgrana@gmail.com>
2010-06-03 12:42:07 -0700

    fix first boot test in example default file

--- default/setup.sh
+++ default/setup.sh
@@ -2,7 +2,7 @@
 
 kuzushi-setup  ## this will wait for all the volumes to attach and take care of mounting and formatting them
 
-if [ "$JUDO_FIRST_BOOT" ] ; then
+if [ "$JUDO_FIRST_BOOT" = "true" ] ; then
   ## do some setup on the first boot only
 fi
 
